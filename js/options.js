937d10414530753ace2a3cd9db18d4a77b5cae39
Jakson Rochelly <jrochelly@gmail.com>
2013-06-18 13:59:22 -0300

    Removed name of page

--- js/options.js
+++ js/options.js
@@ -2,29 +2,28 @@ function getOptions(){
 	chrome.storage.sync.get('links', function(r){
 		links = r.links;
 
-		for (name in links){
-			$('#list').append('<div class="link"><div class="division"><label>Name:</label><input name="link_name" class="link_name" type="text" value="'+name+'"placeholder="Site name" required></div><div class="division"><label>URL:</label><input name="link_url" class="link_url" type="text" value="'+links[name]+'" placeholder="http://www..." required pattern="https?://.+"></div></div>');  
+		for (url in links){
+			$('#list').append('<div class="link"><div class="division"><label>URL:</label><input name="link_url" class="link_url" type="url" value="'+links[url]+'" placeholder="http://www..."></div></div>');  
 		};
 		if (!links) {
 			$("#add_form").fadeIn( "fast" );
 		  for (var i = 1; i <= 3; i++) {
-		    $('#list').append('<div class="link"><div class="division"><label>Name:</label><input name="link_name" class="link_name" type="text" placeholder="Site name" required></div><div class="division"><label>URL:</label><input name="link_url" class="link_url" type="text" placeholder="http://www..." required pattern="https?://.+"></div></div>');  
+		    $('#list').append('<div class="link"><div class="division"><label>URL:</label><input name="link_url" class="link_url" type="url" placeholder="http://www..."></div><a class="remove" href="#"></div>');  
 		  };
 		};
 	});
 }
 
 $(function(){
-		var links = {};
+		var links = [];
 		$('#flash').hide();
 		getOptions();
 		$('form').submit(function (){
 			$('#list .link').each(function(){
-				link_name = $(this).children().children('.link_name').val();
 				url = $(this).children().children('.link_url').val();
 				// Cannot be empty
-				if (link_name && url) {
-					links[link_name] = url;
+				if (url) {
+					links.push(url);
 				};
 			});
 			// Save them all!
@@ -37,7 +36,7 @@ $(function(){
 // Add Link to list
   $('#add_link_list').click(function(){
     i = $('#list .link').length + 1;
-		j = $('<div class="link"><div class="division"><label>Name:</label><input name="link_name" class="link_name" type="text" placeholder="Site name" required></div><div class="division"><label>URL:</label><input name="link_url" class="link_url" type="text" placeholder="http://www..." required pattern="https?://.+"></div></div>').hide().fadeIn(500);
+		j = $('<div class="link"><div class="division"><label>URL:</label><input name="link_url" class="link_url" type="url" placeholder="http://www..."></div></div>').hide().fadeIn(500);
 		$('#list').append(j);
   });
 });
