25979c811dd8e909fc79ed04175d4444c6f31e0e
Jakson Rochelly <jrochelly@gmail.com>
2015-07-01 00:20:56 -0300

    New Layout | Align structure to the one live

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 ##Open several webpages at once with Multilinks Shortcut
 It's simple and easy.
+
+
+
+Get the extensions at [Chrome Web Store](https://chrome.google.com/webstore/detail/ddfoagnhebpbidjkganhgninciiecgon).
--- js/background.js
+++ js/background.js
@@ -1,10 +1,18 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-  var links = {}
+function open_links(){
+  var links = {};
   chrome.storage.sync.get('links', function(result){
-    links = result.links;
-    for (name in links){
-      chrome.tabs.create({url:links[name]});
-    };
-    
+  links = result.links;
+  for (name in links){
+    chrome.tabs.create({url:links[name]});
+  };
+
   });
+}
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  open_links();
+});
+
+chrome.commands.onCommand.addListener(function(command) {
+  open_links();
 });
