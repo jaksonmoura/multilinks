8c9cc0744960867d9b92da949ad29bcde17facde
Jakson Rochelly <jrochelly@gmail.com>
2015-07-01 22:31:56 -0300

    Finished design | New shortcut | New icon

--- js/options.js
+++ js/options.js
@@ -1,14 +1,8 @@
 $(document).ready(function(){
-	if (navigator.platform == "MacIntel") {
-		$('#shortcut').html('<b>&#x2318;+Shift+M</b> to open your links.');
-	} else {
-		$('#shortcut').html('<b>Alt+Shift+M</b> to open your links.');
-	}
-
 	// Add Link to list
 	$('#add_link_list').click(function(){
 	  i = $('#list .link').length + 1;
-		j = $('<div class="link"><div class="division"><label>URL:</label><input name="link_url" class="link_url" type="url" placeholder="http://www..."><a class="remove" href="#">-</a></div></div>').hide().fadeIn(500);
+		j = $('<div class="link"><div class="division"><input name="link_url" class="link_url" type="url" placeholder="http://www..."><a class="remove" href="#"></a></div></div>').hide().fadeIn(500);
 		$('#list').append(j);
 	});
 	// Remove link
@@ -22,12 +16,12 @@ function getOptions(){
 		links = r.links;
 
 		for (url in links){
-			$('#list').append('<div class="link"><div class="division"><label>URL:</label><input name="link_url" class="link_url" type="url" value="'+links[url]+'" placeholder="http://www..."><a class="remove" href="#">-</a></div></div>');
+			$('#list').append('<div class="link"><div class="division"><input name="link_url" class="link_url" type="url" value="'+links[url]+'" placeholder="http://www..."><a class="remove" href="#"></a></div></div>');
 		};
 		if (!links) {
 			$("#add_form").fadeIn( "fast" );
 		  for (var i = 1; i <= 3; i++) {
-		    $('#list').append('<div class="link"><div class="division"><label>URL:</label><input name="link_url" class="link_url" type="url" placeholder="http://www..."><a class="remove" href="#">-</a></div></div>');
+		    $('#list').append('<div class="link"><div class="division"><input name="link_url" class="link_url" type="url" placeholder="http://www..."><a class="remove" href="#"></a></div></div>');
 		  };
 		};
 	});
